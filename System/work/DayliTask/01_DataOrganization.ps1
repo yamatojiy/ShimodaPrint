f97516d47c4f5e0173337a2c6f476364bf74118e
yamatoji_yohei_shimodaprint <y.yamatoji@shimoda-print.co.jp>
2024-07-31 15:13:00 +0900

    ■処理追加 ・工程完了の情報も追加しました。

--- System/work/DayliTask/01_DataOrganization.ps1
+++ System/work/DayliTask/01_DataOrganization.ps1
@@ -1,2 +1,10 @@
-# ファイルを実行
-Start-Process  "X:\system\works\DailyProcessing\DailyOpsHub.cmx"
+# 01_DataOrganization.ps1
+
+# スクリプトのディレクトリを取得
+$scriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+
+# OpsHub.cmx のパスを組み立てる
+$OpsHubPath = Join-Path -Path $scriptDirectory -ChildPath "DailyOpsHub.cmx"
+
+# OpsHub.cmx を実行する
+Start-Process -FilePath $OpsHubPath
---
+++ System/work/DayliTask/02_DataOrganization.ps1
@@ -0,0 +1,3 @@
+# close_kiri.ps1
+Start-Sleep -Seconds 250
+Stop-Process -Name "KIRI10" -Force
