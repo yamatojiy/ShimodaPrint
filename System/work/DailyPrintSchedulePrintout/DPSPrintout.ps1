846615c35b413d2700f3be25e80bff88de999d54
yamatoji_yohei_shimodaprint <y.yamatoji@shimoda-print.co.jp>
2024-11-07 10:07:37 +0900

    ■2025年カレンダーを更新 ■毎日のタスクをPDFにするcmxを作成

---
+++ System/work/DailyPrintSchedulePrintout/DPSPrintout.ps1
@@ -0,0 +1,14 @@
+#DPSPrintout.ps1
+
+# スクリプトのディレクトリを取得
+$scriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+
+# OpsHub.cmx のパスを組み立てる
+$OpsHubPath = Join-Path -Path $scriptDirectory -ChildPath "DailyPrintSchedulePrintout.cmx"
+
+# OpsHub.cmx を実行する
+Start-Process -FilePath $OpsHubPath
+
+# close_kiri.ps1
+Start-Sleep -Seconds 900
+Stop-Process -Name "KIRI10" -Force
